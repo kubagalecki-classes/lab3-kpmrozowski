bad11fa94820895ab8831f0ab3498721f7a73245
kpmrozowski <kpmrozowski@gmail.com>
2020-11-16 23:59:29 +0000

    //cout

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <iostream>
 #include "include/Stocznia.hpp"
 unsigned int transportujFlota(unsigned int towar)
 {
@@ -8,24 +9,25 @@ unsigned int transportujFlota(unsigned int towar)
     Stocznia stocznia{};
     std::vector< Statek* > myShips;
     //Statek*  s1 = stocznia();
-    std::cout << "towar = " << towar << std::endl;
-    std::cout << "Stocznia::getTotalCap() = " << Stocznia::getTotalCap() << std::endl << std::endl;
-    
-    for (int i = 1; towar >= Stocznia::getTotalCap(); i++) {
+    //std::cout << "towar = " << towar << std::endl;
+    //std::cout << "Stocznia::getTotalCap() = " << Stocznia::getTotalCap() << std::endl << std::endl;
+    unsigned int zaladowanie = 0;
+    for (int i = 1; towar >= zaladowanie; i++) {
         Statek* s = stocznia();
+				zaladowanie = zaladowanie + s->transportuj();
         myShips.push_back(s);
-        std::cout << "Statek nr " << i;
+        //std::cout << "Statek nr " << i;
         Statek* czyZaglowiec = dynamic_cast< Zaglowiec* >(s);
         Statek* czyTankowiec = dynamic_cast< Tankowiec* >(s);
-        if (czyZaglowiec) {std::cout << "\tto Zaglowiec!";}
-        if (czyTankowiec) {std::cout << "\tto Tankowiec!";}
-        unsigned int capac = myShips[i]->transportuj();
-        std::cout << " Capacity: " << capac;
-        std::cout << " Stocznia::getTotalCap() = " << Stocznia::getTotalCap() << "\n";
-				delete s;
+        if (czyZaglowiec){}//{std::cout << "\tto Zaglowiec!";}
+        if (czyTankowiec){}//{std::cout << "\tto Tankowiec!";}
+        //unsigned int capac = myShips[i]->transportuj();
+        //std::cout << " Capacity: " << capac;
+        //std::cout << " Stocznia::getTotalCap() = " << Stocznia::getTotalCap() << "\n";
+				//delete s;
     }
     unsigned int zaglowce = CountThis< Zaglowiec >::get();
-    std::cout << "Zaglowce: " << zaglowce << "\n";
-    myShips.clear();
+    //std::cout << "Zaglowce: " << zaglowce << "\n";
+    //myShips.clear();
     return zaglowce;
 }
